64f6a3c12ee217695bbfba74e9ee402ce9ca1b09
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2025-09-15 00:00:41 -0600

    Limpiar repositorio y añadir proyecto NatureAPI completo

--- LibraryAPI/obj/Debug/net9.0/LibraryAPI.AssemblyInfo.cs
+++ LibraryAPI/obj/Debug/net9.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ee78d9cf72a5bfe51f4558501f5f6b09d798db")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
