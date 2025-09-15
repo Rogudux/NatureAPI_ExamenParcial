519d795521c8af253897f33ea847b48e36240a31
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2025-09-14 23:51:58 -0600

    API completa

---
+++ LibraryAPI/models/entities/Amenity.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryAPI.models.entities;
+
+public class Amenity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    
+}
